eb358ff5637c48e5c466a34f7675f95117679951
Jisu Kim <hellojs242@gmail.com>
2023-03-01 16:53:15 +0900

    :sparkles: feat : mariadb config 제외하고 완성

--- srcs/requirements/mariadb/docker-entrypoint.sh
+++ srcs/requirements/mariadb/docker-entrypoint.sh
@@ -21,39 +21,38 @@ print_configs() {
 
 check_db_exists() {
   DATABASE_ALREADY_EXISTS=
-  if [ -d "/var/lib/mysql/mysql" ]; then
+  if [ -d "${DATADIR}mysql" ]; then
     DATABASE_ALREADY_EXISTS='true'
   fi
 }
 
 check_minimum_env() {
   if [ -z $MYSQL_ROOT_PASSWORD ] || [ -z $MYSQL_USER ] || [ -z $MYSQL_PASSWORD ]; then
-    logger_error 'error: database is uninitialized and password option is not specified '
+    logger_error 'database is uninitialized and password option is not specified '
   fi
 }
 
-# run command as root@localhost
+# run command as mysql user
 exec_client() {
-  mariadb -umysql "$@"
+  mariadb --protocol=socket -umysql -hlocalhost --socket="${SOCKET}" "$@"
 }
 
-kill_server() {
+kill_server_for_init() {
   kill "$MARIADB_PID"
   wait "$MARIADB_PID"
 }
 
 run_server_for_init() {
   # run as background process
-  "$@" --skip-networking --socket="/var/run/mysqld/mysqld.sock" \
+  "$@" --skip-networking --socket="$SOCKET" \
   --loose-innodb_buffer_pool_load_at_startup=0 &
   MARIADB_PID=$!
 
   logger_info "Waiting for server startup ..."
-
   # only use the root password if the database has already been initialized
 	# so that it won't try to fill in a password file when it hasn't been set yet
   local i
-  # decreasing 30 to 0 until timeout (30 seconds)
+  # waiting server until timeout (30 seconds)
 	for i in `seq 0 30` ; do
     echo $i th try
 		if echo "SELECT 1" | exec_client --database=mysql; then
@@ -66,10 +65,6 @@ run_server_for_init() {
   fi
   logger_info "Connected to server successfully!"
 }
-
-#stop_server_for_init() {
-#}
-
 
 #sql_escape_string_literal() {
 #  local newline=$'\n' # real newline
@@ -105,21 +100,25 @@ if [ "$1" = 'mariadbd' ] || [ "$1" = 'mysqld' ]; then
 
   print_configs 'datadir' "$@"
   print_configs 'socket' "$@"
+  # TODO : datadir, socket extract logic
+  DATADIR="/var/lib/mysql/"
+  SOCKET="/var/run/mysqld/mysqld.sock"
   check_db_exists
 
   # root
   if [ "$(id -u)" = '0' ]; then
     # 이미 존재..하지 않으면 삭제.
     if [ -z $DATABASE_ALREADY_EXISTS ]; then
-      rm -rf /var/lib/mysql/*
+      rm -rf "${DATADIR}*"
     fi
-    mkdir -p /var/lib/mysql /var/run/mysqld
-    # 무조건 바꾸는 게 아니라 소유자가 mysql 이 아닌 파일들만 찾아서 권한을 바꾼다. 진짜 또라이아님?
-    find "/var/lib/mysql/" \! -user mysql -exec chown mysql: '{}' +
+    mkdir -p "$DATADIR" "${SOCKET%/*}"
+    # 무조건 바꾸는 게 아니라 소유자가 mysql 이 아닌 파일들만 찾아서 권한을 바꾼다.
+    find "${DATADIR}" \! -user mysql -exec chown mysql: '{}' +
     # /var/run/mysqld 의 권한만 바꾼다.
-    find "/var/run/mysqld/" -maxdepth 0 \! -user mysql -exec chown mysql: '{}' \;
-    # chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
+    find "${SOCKET%/*}" -maxdepth 0 \! -user mysql -exec chown mysql: '{}' \;
+    # chown -R mysql:mysql ${DATADIR} /var/run/mysqld
     # 얘는 안에 있는 모든 file 을 재귀적으로 권한바꿈.
+    # user를 mysql 로 변경하여 현재 스크립트를 재실행한다.
     exec su-exec mysql "$0" "$@"
   fi
 
@@ -127,14 +126,19 @@ if [ "$1" = 'mariadbd' ] || [ "$1" = 'mysqld' ]; then
 
   if [ -z $DATABASE_ALREADY_EXISTS ]; then
     # mysql user 를 auth-root-socket-user 로 생성해서 바로 접속가능. (아마도 ^^)
-    mysql_install_db --datadir="/var/lib/mysql/" --auth-root-socket-user=mysql
+    mysql_install_db --datadir="${DATADIR}"\
+     '--skip-test-db' '--auth-root-socket-user=mysql'
     setup_db "$@"
 
-    kill_server
+    kill_server_for_init
   fi
-
 fi
 
-exec "$@" # 확장성 - COMMAND
+# TODO : edit configures
+# 1. skip-networking=false
 
-#/bin/sh
+if [ "$1" = "mariadbd" ] ; then
+  set -- $@  "--skip-networking=false"
+fi
+
+exec "$@"
