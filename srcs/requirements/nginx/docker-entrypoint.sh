da74ea478125866f431cb65d7ae69316feea8a33
Jisu Kim <hellojs242@gmail.com>
2023-03-11 00:24:15 +0900

    :sparkles: change nginx user to root

--- srcs/requirements/nginx/docker-entrypoint.sh
+++ srcs/requirements/nginx/docker-entrypoint.sh
@@ -7,26 +7,17 @@ logger_info() {
   printf "\033[0m"
 }
 
-logger_error() {
-  printf "\033[31m"
-  echo "[$(date +'%Y-%m-%d %H:%M:%S')] [ERROR] $@" >&2
-  printf "\033[0m"
-  exit 1
-}
-
 if [ "$1" = 'nginx' ]; then
-  if [ "$(id -u)" = '0' ]; then
-    find /var/lib/nginx \! -user www-data -exec chown www-data '{}' +
-    cd /var/lib/nginx && exec su-exec www-data "$0" "$@"
-  fi;
-
+  find /var/lib/nginx \! -user www-data -exec chown www-data '{}' +
   if [ -f /var/lib/nginx/keys/server.crt ] && [ -f /var/lib/nginx/keys/private.key ]; then
     logger_info "SSL certificate already exists."
   else
     logger_info "Generating SSL certificate ..."
     openssl genrsa -out /var/lib/nginx/keys/private.key;
-    openssl req -new -key /var/lib/nginx/keys/private.key -out /var/lib/nginx/keys/server.csr -subj "/";
-    openssl x509 -req -days 365 -in /var/lib/nginx/keys/server.csr -signkey /var/lib/nginx/keys/private.key -out /var/lib/nginx/keys/server.crt;
+    openssl req -new -key /var/lib/nginx/keys/private.key \
+    -out /var/lib/nginx/keys/server.csr -subj "/" >/dev/null 2>&1;
+    openssl x509 -req -days 365 -in /var/lib/nginx/keys/server.csr \
+    -signkey /var/lib/nginx/keys/private.key -out /var/lib/nginx/keys/server.crt >/dev/null 2>&1;
     rm -rf server.csr;
   fi
 fi
--- srcs/requirements/wordpress/docker-entrypoint.sh
+++ srcs/requirements/wordpress/docker-entrypoint.sh
@@ -7,13 +7,6 @@ logger_info() {
   printf "\033[0m"
 }
 
-logger_error() {
-  printf "\033[31m"
-  echo "[$(date +'%Y-%m-%d %H:%M:%S')] [ERROR] $@" >&2
-  printf "\033[0m"
-  exit 1
-}
-
 check_minimum_env() {
     if [ -z "$WP_DB_HOST" ] || [ -z "$WP_DB_NAME " ] || [ -z "$WP_DB_USER" ] || [ -z "$WP_DB_PASSWORD" ] \
      || [ -z "$WP_ADMIN_USER" ] || [ -z "$WP_ADMIN_PASSWORD" ] || [ -z "$WP_ADMIN_EMAIL" ] ; then
@@ -24,12 +17,12 @@ check_minimum_env() {
 
 if [ "$1" = 'php-fpm8' ]; then
     if [ "$(id -u)" = '0' ]; then
-        mkdir -p /.wp-cli /var/log/php8 && chown www-data /.wp-cli /var/log/php8
+        mkdir -p /home/www-data/.wp-cli /var/log/php8
+        chown www-data /home/www-data/.wp-cli /var/log/php8
         find /wordpress \! -user www-data -exec chown www-data '{}' +
         cd /wordpress && exec su-exec www-data "$0" "$@"
     fi;
     check_minimum_env
-
 
     if ! [ -f /wordpress/.wp_downloaded ] ; then
         logger_info "downloading wordpress..."
@@ -43,9 +36,9 @@ if [ "$1" = 'php-fpm8' ]; then
     if ! wp core is-installed ; then
         logger_info "installing wordpress..."
         wp core install --url="${DOMAIN_NAME}" --title="인셉션쇼로숑숑" \
-        --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} \
-        --admin_email=${WP_ADMIN_EMAIL} --skip-email
-        wp user create ${WP_USER} ${WP_USER}@${DOMAIN_NAME} --user_pass=${WP_PASSWORD}
+        --admin_user="${WP_ADMIN_USER}" --admin_password="${WP_ADMIN_PASSWORD}" \
+        --admin_email="${WP_ADMIN_EMAIL}" --skip-email
+        wp user create "${WP_USER}" "${WP_USER}@${DOMAIN_NAME}" --user_pass="${WP_PASSWORD}"
         logger_info "wordpress installed!"
     else
         logger_info "wordpress is already installed!"
